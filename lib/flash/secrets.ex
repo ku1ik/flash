c090757d37316c98866e29615422f7cbf993fe16
Marcin Kulik <m@ku1ik.com>
2020-04-28 23:05:21 +0200

    Encrypt stored secrets with AES256-GCM

--- lib/flash/secrets.ex
+++ lib/flash/secrets.ex
@@ -15,9 +15,10 @@ defmodule Flash.Secrets do
     String.trim(text) != ""
   end
 
-  defp do_create_secret(text, ttl) do
+  defp do_create_secret(plain_text, ttl) do
     id = Nanoid.generate()
-    Redix.command!(Redix, ["SET", key(id), text])
+    payload = encrypt(plain_text)
+    Redix.command!(Redix, ["SET", key(id), payload])
 
     if ttl do
       Redix.command!(Redix, ["EXPIRE", key(id), ttl])
@@ -27,7 +28,9 @@ defmodule Flash.Secrets do
   end
 
   def get_secret(id) do
-    Redix.command!(Redix, ["GET", key(id)])
+    if payload = Redix.command!(Redix, ["GET", key(id)]) do
+      decrypt(payload)
+    end
   end
 
   def delete_secret!(id) do
@@ -37,4 +40,34 @@ defmodule Flash.Secrets do
   end
 
   defp key(id), do: "secret:" <> id
+
+  @aad "AES256GCM"
+
+  defp encrypt(plain_text) do
+    iv = :crypto.strong_rand_bytes(16)
+
+    {cipher_text, tag} =
+      :crypto.block_encrypt(:aes_gcm, encryption_key(), iv, {@aad, plain_text, 16})
+
+    Base.encode64(iv <> tag <> cipher_text)
+  end
+
+  defp decrypt(payload) do
+    payload = Base.decode64!(payload)
+    <<iv::binary-16, tag::binary-16, cipher_text::binary>> = payload
+
+    :crypto.block_decrypt(:aes_gcm, encryption_key(), iv, {@aad, cipher_text, tag})
+  end
+
+  defp encryption_key do
+    key =
+      :flash
+      |> Application.get_env(FlashWeb.Endpoint)
+      |> Keyword.get(:secret_key_base)
+      |> Base.decode64!()
+
+    <<key::binary-size(32), _rest::binary>> = key
+
+    key
+  end
 end

214be2a5bd63361b477b9c89342a05b0f5a44700
Marcin Kulik <m@ku1ik.com>
2020-05-01 18:01:18 +0200

    Serve gzipped assets

--- lib/flash_web/endpoint.ex
+++ lib/flash_web/endpoint.ex
@@ -23,7 +23,7 @@ defmodule FlashWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :flash,
-    gzip: false,
+    gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the

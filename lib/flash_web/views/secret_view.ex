8d5b8260b4a7a2b634246752b4ae43de5707dfbf
Marcin Kulik <m@ku1ik.com>
2020-05-03 20:55:52 +0200

    Show cmd key hint only on mac OS

--- lib/flash_web/views/secret_view.ex
+++ lib/flash_web/views/secret_view.ex
@@ -1,3 +1,12 @@
 defmodule FlashWeb.SecretView do
   use FlashWeb, :view
+
+  def cmd_key_available?(conn) do
+    with {_, user_agent} <- List.keyfind(conn.req_headers, "user-agent", 0),
+         true <- String.contains?(user_agent, "Macintosh") do
+      true
+    else
+      _ -> false
+    end
+  end
 end

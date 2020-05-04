830bbb9713e27ddeca6106c817a8527f75a12a81
Marcin Kulik <m@ku1ik.com>
2020-05-04 15:02:20 +0200

    protect_from_forgery plug is not need when SameSite cookies are used

--- lib/flash_web/router.ex
+++ lib/flash_web/router.ex
@@ -6,7 +6,6 @@ defmodule FlashWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
-    plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
 

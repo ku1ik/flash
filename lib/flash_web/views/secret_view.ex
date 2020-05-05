70fd622877b6bdc1051f432bc81655237eef8ecd
Marcin Kulik <m@ku1ik.com>
2020-05-05 18:11:52 +0200

    Platform specific command-line copy instructions

--- lib/flash_web/views/secret_view.ex
+++ lib/flash_web/views/secret_view.ex
@@ -1,7 +1,17 @@
 defmodule FlashWeb.SecretView do
   use FlashWeb, :view
 
-  def cmd_key_available?(conn) do
+  def cmd_key_available?(conn), do: macos?(conn)
+
+  def copy_tool(conn) do
+    if macos?(conn), do: "pbcopy", else: "xclip"
+  end
+
+  def copy_cmd(conn) do
+    if macos?(conn), do: "pbcopy", else: "xclip -selection clipboard"
+  end
+
+  defp macos?(conn) do
     with {_, user_agent} <- List.keyfind(conn.req_headers, "user-agent", 0),
          true <- String.contains?(user_agent, "Macintosh") do
       true

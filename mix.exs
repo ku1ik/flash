93dbdffe4a6403d513dee6a9dbada63ecaa28c8b
Marcin Kulik <m@ku1ik.com>
2020-06-29 16:25:42 +0200

    Add os_mon app for LiveDashboard

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Flash.MixProject do
   def application do
     [
       mod: {Flash.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
